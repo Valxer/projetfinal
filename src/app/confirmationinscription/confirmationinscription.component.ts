1b517a50264895e3af7458a0e49aaba2f0fcadd3
Valxer <levanphungk@gmail.com>
2022-07-27 21:27:04 +0200

    inscription finished

--- src/app/confirmationinscription/confirmationinscription.component.ts
+++ src/app/confirmationinscription/confirmationinscription.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirmationinscription',
@@ -8,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class ConfirmationinscriptionComponent implements OnInit {
   numID: any;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  goToConnexion():void {
+    this.router.navigate(['connexion'])
   }
 
 }
