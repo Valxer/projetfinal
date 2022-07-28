afd349ee04229e5c0f785bd726bf3675b8e05a5f
Valxer <levanphungk@gmail.com>
2022-07-28 13:51:10 +0200

    inscription form

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Client } from '../client';
 import { ClientService } from '../client.service';
@@ -11,6 +12,13 @@ import { ClientService } from '../client.service';
 export class InscriptionComponent implements OnInit {
 
   newclient= new Client();
+  profileForm = new FormGroup({
+    id: new FormControl('', Validators.required),
+    nom: new FormControl('', Validators.required),
+    prenom: new FormControl('', Validators.required),
+    adresse: new FormControl('', Validators.required),
+    mdp: new FormControl('', Validators.required),
+  });
 
   constructor(private router: Router,private srv: ClientService) { }
 
@@ -21,8 +29,12 @@ export class InscriptionComponent implements OnInit {
    * Call ClientService.create to create Create a new Client.
    */
   create(){
+    this.newclient.id = this.profileForm.value.id
+    this.newclient.nom = this.profileForm.value.nom
+    this.newclient.prenom = this.profileForm.value.prenom
+    this.newclient.adresse = this.profileForm.value.adresse
+    this.newclient.password = this.profileForm.value.mdp
     this.srv.create(this.newclient);
-    
   }
 
 }
